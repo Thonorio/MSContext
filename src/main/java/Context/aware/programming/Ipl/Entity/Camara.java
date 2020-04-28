f8bdb3afa0a4f6da71bdebcec4e6bb26b9f91fca
Thonorio <tomas.honorio@gmail.com>
2020-04-28 17:25:31 +0100

    Init

--- src/main/java/Context/aware/programming/Ipl/Entity/Camara.java
+++ src/main/java/Context/aware/programming/Ipl/Entity/Camara.java
@@ -1,11 +1,21 @@
 package Context.aware.programming.Ipl.Entity;
 
+import javax.persistence.*;
+
 @Entity
 public class Camara {
     @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(nullable = false)
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/Context/aware/programming/Ipl/IplApplication.java
+++ src/main/java/Context/aware/programming/Ipl/IplApplication.java
@@ -2,16 +2,37 @@ package Context.aware.programming.Ipl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.sputnikdev.bluetooth.URL;
+import org.sputnikdev.bluetooth.manager.CharacteristicGovernor;
+import org.sputnikdev.bluetooth.manager.impl.BluetoothManagerBuilder;
 import tinyb.BluetoothManager;
+
+import java.util.concurrent.ExecutionException;
 
 
 @SpringBootApplication
 public class IplApplication {
 
 	public static void main(String[] args) {
-		IplApplication.class.getProtectionDomain().getCodeSource().getLocation();
-		SpringApplication.run(IplApplication.class, args);
-		BluetoothManager manager = BluetoothManager.getBluetoothManager();
+		//SpringApplication.run(IplApplication.class, args);
+		//IplApplication.class.getProtectionDomain().getCodeSource().getLocation();
+		//BluetoothManager manager = BluetoothManager.getBluetoothManager();
+		try {
+			new BluetoothManagerBuilder()
+					.withTinyBTransport(true)
+					.withBlueGigaTransport("^*.$")
+					.build()
+					.getCharacteristicGovernor(new URL("/XX:XX:XX:XX:XX:XX/F7:EC:62:B9:CF:1F/"
+							+ "0000180f-0000-1000-8000-00805f9b34fb/00002a19-0000-1000-8000-00805f9b34fb"), true)
+					.whenReady(CharacteristicGovernor::read)
+					.thenAccept(data -> {
+						System.out.println("Battery level: " + data[0]);
+					}).get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
